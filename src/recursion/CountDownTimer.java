40be108023331033f934f03531e611a2be8df232
koline2024 <christopherxia2020@gmail.com>
2025-05-12 13:45:46 -0400

    finished problem 1

--- src/recursion/CountDownTimer.java
+++ src/recursion/CountDownTimer.java
@@ -1,11 +1,15 @@
 package recursion;
 
 public class CountDownTimer {
-    public static void countDown(int n) {
-        // solution here
+    public static int countDown(int n) {
+        if(n == 0){
+            return 0;
+        }
+        System.out.print(n + " ");
+        return countDown(n - 1);
     }
 
     public static void main(String[] args) {
-        countDown(5); // Output: 5 4 3 2 1
+        countDown(5); 
     }
 }
