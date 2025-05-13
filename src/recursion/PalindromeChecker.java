8cba64e8441b4443a1446e111ff551e1584a31e1
koline2024 <christopherxia2020@gmail.com>
2025-05-12 21:27:07 -0400

    solved palindrome question

---
+++ src/recursion/PalindromeChecker.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class PalindromeChecker {
+   
+    public static boolean isPalindrome(String s){
+        
+        //recursive case
+        if(s.length() == 1){
+            return true;
+
+        //recursive case;
+        }else if(s.charAt(0) == s.charAt(s.length()-1)){
+          return isPalindrome(s.substring(1, s.length()-1));
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "biaic";
+        System.out.println(isPalindrome(s));
+    }
+
+}
+
+
--- src/recursion/SumofNaturalNumbers.java
+++ src/recursion/SumofNaturalNumbers.java
@@ -14,4 +14,6 @@ public class SumofNaturalNumbers {
     public static void main(String[] args) {
         System.out.println(sum(5)); // Output: 15
     }
+
+    
 }
